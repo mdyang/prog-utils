8cf5813c9e205cbb175b4b811c2e4877aa37e6f0
Mengdong Yang <conanholmes001@gmail.com>
2013-12-20 13:17:26 +0800

    Improve gtest to support C

--- oj-utils/win/gtest.bat
+++ oj-utils/win/gtest.bat
@@ -4,6 +4,12 @@ del test.out
 
 if exist main.cpp goto cpp
 if exist main.cs goto cs
+if exist main.c goto c
+
+:c
+set COMPILE=cl main.c
+set EXEC=main.exe
+goto end
 
 :cpp
 set COMPILE=cl main.cpp
